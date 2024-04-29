d60fe1ba2e54c4798d33916291761b6c246eb596
Ramesh Budumuru <rbudumuru@nisum.com>
2024-04-29 17:31:20 +0530

    initial commit

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,67 @@
+import React, { useContext } from 'react';
+import { useState } from 'react';
+import './Sidebar.css';
+import { assets } from '../../assets/assets';
+import { Context } from '../../context/context';
+
+const Sidebar = () => {
+
+    const [extend, setExtend] = useState(false);
+    const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
+
+    const loadPrompt = async (prompt) => {
+        setRecentPrompt(prompt)
+        await onSent(prompt)
+    }
+
+    return (
+        <div className='sidebar'>
+            <div className="top">
+                <img onClick={
+                    () => {
+                        setExtend(prev => !prev)
+                    }
+                }
+                    className='menu' src={assets.menu_icon} alt="" />
+
+                <div onClick={()=>newChat()}  className="new-chat">
+                    <img src={assets.plus_icon} alt="" />
+                    {extend ? <p>New Chat</p> : null}
+
+                </div>
+
+                {extend ?
+                    <div className="recent">
+                        <p className="recent-title">Recent</p>
+                        {prevPrompts.map((item, index) => {
+                            return (
+                                <div onClick={()=>loadPrompt(item)} className="recent-entry">
+                                    <img src={assets.message_icon} alt="" />
+                                    <p>{item.slice(0,18)}...</p>
+                                </div>
+                            )
+                        })}
+
+                    </div>
+                    : null}
+            </div>
+            <div className="bottom">
+                <div className="bottom-item recent-entry ">
+                    <img src={assets.question_icon} alt="" />
+                    {extend ? <p>Help</p> : null}
+                </div>
+                <div className="bottom-item recent-entry ">
+                    <img src={assets.history_icon} alt="" />
+                    {extend ? <p>Activity</p> : null}
+                </div>
+                <div className="bottom-item recent-entry ">
+                    <img src={assets.setting_icon} alt="" />
+                    {extend ? <p>Settings</p> : null}
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
